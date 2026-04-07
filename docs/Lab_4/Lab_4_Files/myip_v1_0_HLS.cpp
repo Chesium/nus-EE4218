37231133cd7e9640158ae3004e0d096169dea90b
Chesium <chesium@hotmail.com>
2026-04-07 15:23:06 +0800

    feat: lab 4 hls files

--- docs/Lab_4/Lab_4_Files/myip_v1_0_HLS.cpp
+++ docs/Lab_4/Lab_4_Files/myip_v1_0_HLS.cpp
@@ -17,8 +17,12 @@
 #include "ap_int.h"
 #include "ap_axi_sdata.h"
 
-#define NUMBER_OF_INPUT_WORDS 520  // length of an input vector
-#define NUMBER_OF_OUTPUT_WORDS 64  // length of an input vector
+#define A_LEN 512
+#define B_LEN 8
+#define RES_LEN A_LEN / B_LEN
+
+#define NUMBER_OF_INPUT_WORDS A_LEN + B_LEN  // length of an input vector
+#define NUMBER_OF_OUTPUT_WORDS RES_LEN  // length of an input vector
 
 // ACLK, ARESETN, TREADY, TDATA, TVALID are essential signals for AXIS. New version of AXI DMA seems to expect TSTRB and/or TKEEP as well.
 
@@ -31,21 +35,113 @@ void myip_v1_0_HLS(hls::stream<AXIS>& S_AXIS, hls::stream<AXIS>& M_AXIS){
 
 	int word_cnt;
 	ap_uint<8> sum = 0; // using arbitrary precision
-	//int sum = 0;		 // using 32 bit precision
+	// int sum = 0;		 // using 32 bit precision
+
+	ap_uint<19> acc;
+	ap_uint<8> A[A_LEN];
+	ap_uint<8> B[B_LEN];
+	ap_uint<8> res[RES_LEN];
+	#pragma HLS ARRAY_PARTITION variable=B complete dim=1
+	#pragma HLS ARRAY_PARTITION variable=A cyclic factor=8 dim=1
+
 	AXIS read_input, write_output;
 
-		myip_v1_0_HLS_for1:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++){
+		myip_v1_0_HLS_for_input:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++){
+#pragma HLS UNROLL
 			read_input = S_AXIS.read();
 			// read_input is the element (data + other signals) received by our ip through S_AXIS in one clock cycle (which contains one word).
 			// read() extracts it from the stream. Overloaded operator >> can also be used.
-			sum += read_input.data; //extracting that word
+			if (word_cnt < A_LEN)
+				A[word_cnt] = read_input.data;
+			else 
+				B[word_cnt - A_LEN] = read_input.data;
+			// sum += read_input.data; //extracting that word
 			// We are not making using of S_AXIS_TLAST in this example.
 			// S_AXIS_TLAST is required only when we are receiving an unknown number of words.
 		}
+		
+  		myip_v1_0_HLS_for_calc:
+		for (int i = 0; i < RES_LEN; ++i) {
+			#pragma HLS PIPELINE II=1
+    		acc = 0;
+    		int base = i * B_LEN;
+    		for (int j = 0; j < B_LEN; ++j) {
+			#pragma HLS UNROLL
+      			acc += A[base + j] * B[j];
+    		}
+    		res[i] = acc.range(15,8);
+  		}
 
-		myip_v1_0_HLS_for2:for(word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++){
+		myip_v1_0_HLS_output:for(word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++){
+			#pragma HLS pipeline off
 			//write_output.data = sum.to_int() + word_cnt;	// using arbitrary precision internally but int for interfacing
-			write_output.data = sum + word_cnt;	// using 32 bit precision or arbitrary precision all the way
+			write_output.data = res[word_cnt];	// using 32 bit precision or arbitrary precision all the way
+			// write_output is the element sent by our ip through M_AXIS in one clock cycle.
+			write_output.last = 0;
+			write_output.keep = 0xFU;
+			write_output.strb = 0xFU;
+			if(word_cnt==NUMBER_OF_OUTPUT_WORDS-1)
+			{
+				write_output.last = 1;
+				// M_AXIS_TLAST is required to be asserted for the last word.
+				// Else, the AXI Stream FIFO / AXI DMA will not know if all the words have been received from the co-processor.
+			}
+			M_AXIS.write(write_output);
+			// write() inserts it into the stream. Overloaded operator << can also be used.
+		}
+}
+
+void myip_v1_0_HLS_unoptimized(hls::stream<AXIS>& S_AXIS, hls::stream<AXIS>& M_AXIS){
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE axis port=S_AXIS
+#pragma HLS INTERFACE axis port=M_AXIS
+
+	int word_cnt;
+	ap_uint<8> sum = 0; // using arbitrary precision
+	// int sum = 0;		 // using 32 bit precision
+
+	ap_uint<19> acc;
+	ap_uint<8> A[A_LEN];
+	ap_uint<8> B[B_LEN];
+	ap_uint<8> res[RES_LEN];
+	// #pragma HLS ARRAY_PARTITION variable=B complete dim=1
+	// #pragma HLS ARRAY_PARTITION variable=A cyclic factor=8 dim=1
+
+	AXIS read_input, write_output;
+
+		myip_v1_0_HLS_for_input:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++){
+			#pragma HLS pipeline off
+// #pragma HLS UNROLL
+			read_input = S_AXIS.read();
+			// read_input is the element (data + other signals) received by our ip through S_AXIS in one clock cycle (which contains one word).
+			// read() extracts it from the stream. Overloaded operator >> can also be used.
+			if (word_cnt < A_LEN)
+				A[word_cnt] = read_input.data;
+			else 
+				B[word_cnt - A_LEN] = read_input.data;
+			// sum += read_input.data; //extracting that word
+			// We are not making using of S_AXIS_TLAST in this example.
+			// S_AXIS_TLAST is required only when we are receiving an unknown number of words.
+		}
+		
+  		myip_v1_0_HLS_for_calc:
+		for (int i = 0; i < RES_LEN; ++i) {
+			#pragma HLS pipeline off
+			// #pragma HLS PIPELINE II=1
+    		acc = 0;
+    		int base = i * B_LEN;
+    		for (int j = 0; j < B_LEN; ++j) {
+			#pragma HLS pipeline off
+			// #pragma HLS UNROLL
+      			acc += A[base + j] * B[j];
+    		}
+    		res[i] = acc.range(15,8);
+  		}
+
+		myip_v1_0_HLS_output:for(word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++){
+			#pragma HLS pipeline off
+			//write_output.data = sum.to_int() + word_cnt;	// using arbitrary precision internally but int for interfacing
+			write_output.data = res[word_cnt];	// using 32 bit precision or arbitrary precision all the way
 			// write_output is the element sent by our ip through M_AXIS in one clock cycle.
 			write_output.last = 0;
 			write_output.keep = 0xFU;
--- docs/Lab_4/Lab_4_Files/test_myip_v1_0_HLS.cpp
+++ docs/Lab_4/Lab_4_Files/test_myip_v1_0_HLS.cpp
@@ -1,121 +1,162 @@
 /*
 ----------------------------------------------------------------------------------
 --	(c) Rajesh C Panicker, NUS,
---  Description : Self-checking testbench for AXI Stream Coprocessor (HLS) implementing the sum of 4 numbers
+--  Description : Self-checking testbench for AXI Stream Coprocessor (HLS)
+implementing the sum of 4 numbers
 --	License terms :
 --	You are free to use this code as long as you
---		(i) DO NOT post a modified version of this on any public repository;
+--		(i) DO NOT post a modified version of this on any public
+repository;
 --		(ii) use it only for educational purposes;
---		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of any entity.
---		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
---		(v) send an email to rajesh.panicker@ieee.org briefly mentioning its use (except when used for the course EE4218/CEG5203 at the National University of Singapore);
---		(vi) retain this notice in this file or any files derived from this.
+--		(iii) accept the responsibility to ensure that your
+implementation does not violate any intellectual property of any entity.
+--		(iv) accept that the program is provided "as is" without
+warranty of any kind or assurance regarding its suitability for any particular
+purpose;
+--		(v) send an email to rajesh.panicker@ieee.org briefly mentioning
+its use (except when used for the course EE4218/CEG5203 at the National
+University of Singapore);
+--		(vi) retain this notice in this file or any files derived from
+this.
 ----------------------------------------------------------------------------------
 */
 
-#include <stdio.h>
-#include "hls_stream.h"
 #include "ap_axi_sdata.h"
+#include "data.hpp"
+#include "hls_stream.h"
+#include <stdio.h>
 
-typedef ap_axis<32,0,0,0> AXIS;
+typedef ap_axis<32, 0, 0, 0> AXIS;
 
 /***************** Coprocessor function declaration *********************/
 
-void myip_v1_0_HLS(hls::stream<AXIS>& S_AXIS, hls::stream<AXIS>& M_AXIS);
-
+void myip_v1_0_HLS(hls::stream<AXIS> &S_AXIS, hls::stream<AXIS> &M_AXIS);
 
 /***************** Macros *********************/
-#define NUMBER_OF_INPUT_WORDS 4  // length of an input vector
-#define NUMBER_OF_OUTPUT_WORDS 4  // length of an input vector
-#define NUMBER_OF_TEST_VECTORS 2  // number of such test vectors (cases)
 
+#define A_LEN 512
+#define B_LEN 8
+#define RES_LEN A_LEN / B_LEN
+
+#define NUMBER_OF_INPUT_WORDS A_LEN + B_LEN // length of an input vector
+#define NUMBER_OF_OUTPUT_WORDS RES_LEN      // length of an input vector
+
+#define NUMBER_OF_TEST_VECTORS 1 // number of such test vectors (cases)
 
 /************************** Variable Definitions *****************************/
-int test_input_memory [NUMBER_OF_TEST_VECTORS*NUMBER_OF_INPUT_WORDS] = {0x01, 0x02, 0x03, 0x04, 0x02, 0x03, 0x04, 0x05}; // 4 inputs * 2
-int test_result_expected_memory [NUMBER_OF_TEST_VECTORS*NUMBER_OF_OUTPUT_WORDS];// 4 outputs *2
-int result_memory [NUMBER_OF_TEST_VECTORS*NUMBER_OF_OUTPUT_WORDS]; // same size as test_result_expected_memory
+int test_input_memory[NUMBER_OF_TEST_VECTORS *
+                      NUMBER_OF_INPUT_WORDS]; // 4 inputs * 2
+int test_result_expected_memory[NUMBER_OF_TEST_VECTORS *
+                                NUMBER_OF_OUTPUT_WORDS]; // 4 outputs *2
+int result_memory[NUMBER_OF_TEST_VECTORS *
+                  NUMBER_OF_OUTPUT_WORDS]; // same size as
+                                           // test_result_expected_memory
 
 /*****************************************************************************
-* Main function
-******************************************************************************/
-int main()
-{
-	int word_cnt, test_case_cnt = 0;
-	int success;
-	AXIS read_output, write_input;
-	hls::stream<AXIS> S_AXIS;
-	hls::stream<AXIS> M_AXIS;
+ * Main function
+ ******************************************************************************/
+int main() {
+  int word_cnt;
+  int success;
+  AXIS read_output, write_input;
+  hls::stream<AXIS> S_AXIS;
+  hls::stream<AXIS> M_AXIS;
+  // instead of hard-coding the results in test_result_expected_memory
+  // for (test_case_cnt=0 ; test_case_cnt < NUMBER_OF_TEST_VECTORS ;
+  // test_case_cnt++){ 	for (word_cnt=0 ; word_cnt < NUMBER_OF_INPUT_WORDS ;
+  // word_cnt++){ 		sum
+  // +=test_input_memory[word_cnt+test_case_cnt*NUMBER_OF_INPUT_WORDS];
+  // 	}
+  // 	for (word_cnt=0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++) {
+  // 		test_result_expected_memory[word_cnt+test_case_cnt*NUMBER_OF_OUTPUT_WORDS]
+  // = sum + word_cnt;
+  // 	}
+  // }
 
-	/************** Run a software version of the hardware function to validate results ************/
-	// instead of hard-coding the results in test_result_expected_memory
-	int sum;
-	for (test_case_cnt=0 ; test_case_cnt < NUMBER_OF_TEST_VECTORS ; test_case_cnt++){
-		sum = 0;
-		for (word_cnt=0 ; word_cnt < NUMBER_OF_INPUT_WORDS ; word_cnt++){
-			sum +=test_input_memory[word_cnt+test_case_cnt*NUMBER_OF_INPUT_WORDS];
-		}
-		for (word_cnt=0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++) {
-			test_result_expected_memory[word_cnt+test_case_cnt*NUMBER_OF_OUTPUT_WORDS] = sum + word_cnt;
-		}
-	}
+  for (int i = 0; i < A_LEN; i++) {
+    test_input_memory[i] = data_A[i];
+    // if (i < 8)
+    //   printf("set input %d : %d\r\n", i, test_input_memory[i]);
+  }
+    for (int i = 0; i < B_LEN; i++) {
+      test_input_memory[i + A_LEN] = data_B[i];
+    }
 
+  /************** Run a software version of the hardware function to validate
+   * results ************/
+  for (int i = 0; i < (int)RES_LEN; ++i) {
+    int acc = 0;
+    int base = i * B_LEN;
+    for (int j = 0; j < (int)B_LEN; ++j) {
+      acc += data_A[base + j] * data_B[j];
+    }
+    test_result_expected_memory[i] = acc >> 8;
+  }
 
-	for (test_case_cnt=0 ; test_case_cnt < NUMBER_OF_TEST_VECTORS ; test_case_cnt++){
+  //   for (test_case_cnt = 0; test_case_cnt < NUMBER_OF_TEST_VECTORS;
+  //    test_case_cnt++) {
 
+  /******************** Input to Coprocessor : Transmit the Data Stream
+   * ***********************/
 
-		/******************** Input to Coprocessor : Transmit the Data Stream ***********************/
+  // printf(" Transmitting Data for test case %d ... \r\n", test_case_cnt);
 
-		printf(" Transmitting Data for test case %d ... \r\n", test_case_cnt);
+  for (word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++) {
+    if (word_cnt < 8)
+      printf("input %d : %d\r\n", word_cnt, test_input_memory[word_cnt]);
+    write_input.data = test_input_memory[word_cnt];
+    write_input.last = 0;
+    if (word_cnt == NUMBER_OF_INPUT_WORDS - 1) {
+      write_input.last = 1;
+      // S_AXIS_TLAST is asserted for the last word.
+      // Actually, doesn't matter since we are not making using of
+      // S_AXIS_TLAST.
+    }
+    S_AXIS.write(write_input); // insert one word into the stream
+  }
 
-		for (word_cnt=0 ; word_cnt < NUMBER_OF_INPUT_WORDS ; word_cnt++){
+  /* Transmission Complete */
 
-			write_input.data = test_input_memory[word_cnt+test_case_cnt*NUMBER_OF_INPUT_WORDS];
-			write_input.last = 0;
-			if(word_cnt==NUMBER_OF_INPUT_WORDS-1)
-			{
-				write_input.last = 1;
-				// S_AXIS_TLAST is asserted for the last word.
-				// Actually, doesn't matter since we are not making using of S_AXIS_TLAST.
-			}
-			S_AXIS.write(write_input); // insert one word into the stream
-		}
+  /********************* Call the hardware function (invoke the co-processor /
+   * ip) ***************/
 
-		/* Transmission Complete */
+  myip_v1_0_HLS(S_AXIS, M_AXIS);
 
-		/********************* Call the hardware function (invoke the co-processor / ip) ***************/
+  /******************** Output from Coprocessor : Receive the Data Stream
+   * ***********************/
 
-		myip_v1_0_HLS(S_AXIS, M_AXIS);
+  // printf(" Receiving data for test case %d ... \r\n", test_case_cnt);
 
+  for (word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++) {
 
-		/******************** Output from Coprocessor : Receive the Data Stream ***********************/
+    read_output = M_AXIS.read(); // extract one word from the stream
+    result_memory[word_cnt] = read_output.data;
+  }
 
-		printf(" Receiving data for test case %d ... \r\n", test_case_cnt);
+  /* Reception Complete */
+  //   }
 
-		for (word_cnt=0 ; word_cnt < NUMBER_OF_OUTPUT_WORDS ; word_cnt++){
+  /************************** Checking correctness of results
+   * *****************************/
 
-			read_output = M_AXIS.read(); // extract one word from the stream
-			result_memory[word_cnt+test_case_cnt*NUMBER_OF_OUTPUT_WORDS] = read_output.data;
-		}
+  success = 1;
 
-		/* Reception Complete */
-	}
+  /* Compare the data send with the data received */
+  printf(" Comparing data ...\r\n");
+  for (word_cnt = 0; word_cnt < NUMBER_OF_TEST_VECTORS * NUMBER_OF_OUTPUT_WORDS;
+       word_cnt++) {
+    printf("[%d] %d ~ %d\n", word_cnt, result_memory[word_cnt],
+           test_result_expected_memory[word_cnt]);
+    success = success & (result_memory[word_cnt] ==
+                         test_result_expected_memory[word_cnt]);
+  }
 
-	/************************** Checking correctness of results *****************************/
+  if (success != 1) {
+    printf("Test Failed\r\n");
+    return 1;
+  }
 
-	success = 1;
+  printf("Test Success\r\n");
 
-	/* Compare the data send with the data received */
-	printf(" Comparing data ...\r\n");
-	for(word_cnt=0; word_cnt < NUMBER_OF_TEST_VECTORS*NUMBER_OF_OUTPUT_WORDS; word_cnt++){
-		success = success & (result_memory[word_cnt] == test_result_expected_memory[word_cnt]);
-	}
-
-	if (success != 1){
-		printf("Test Failed\r\n");
-		return 1;
-	}
-
-	printf("Test Success\r\n");
-
-	return 0;
+  return 0;
 }
